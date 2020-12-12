b3d10e540b782ccc09880a689a1758bcb0d3c64f
Danutzz8 <Marius.Prutean@outlook.com>
2020-12-13 00:28:41 +0200

    first commit

---
+++ sandbox.ts
@@ -0,0 +1,29 @@
+const character = 'mario';
+
+console.log(character);
+
+const inputs = document.querySelectorAll('input')
+
+console.log(inputs);
+
+inputs.forEach((input) => {
+    console.log(input);
+    
+})
+
+let names = ['luigi', 'john', 'ema',2];
+
+names.push(4)
+
+let age: number;
+
+let ninja: (string | number)[] = [];
+
+let ages: any = 32;
+
+const anchor = document.querySelector('a')
+
+console.log(anchor.href);
+anchor
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
